4f87dc8970452ae7e7e1260cfcf9a05dfac976b2
chrisBokotaII <105431097+chrisBokotaII@users.noreply.github.com>
2022-09-14 01:06:32 +0300

    Add files via upload
    first commit

---
+++ navbar/index.js
@@ -0,0 +1,13 @@
+// let container = document.querySelector(".container");
+
+// container.addEventListener(onclick,function(){
+//    let navbar = document.querySelector(".nav-navbar");
+//  navbar.classList.toggle("active");
+
+// })
+var container = document.querySelector(".container");
+container.onclick = function(){
+    var navbar = document.querySelector(".nav-navbar");
+    navbar.classList.toggle("active");
+     
+}
